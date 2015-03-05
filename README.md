8a1c082252cbd7f62aeb0bf5f502580831ff71c0
Michael Westwood <michael@du.st>
2015-03-05 12:45:00 +0000

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # dust-textareaExpand
 a jQuery plugin to make a textarea auto expand as you type.
+
+## What does it do?
+Textareas don't natively grow to fit content. This plugin enables a textarea's height to fit the height of the text within. You can also make the textarea expand x lines before the text reaches the bottom, and you can make it add y lines when it expands.
+
+## How do I use it?
+You need to have a textarea inside a relatively positioned element. That's the only requirement.
+
+So just
+```html
+<div class="textarea-expand">
+	<textarea></textarea>
+</div>
+```
+
+and call the plugin on the element.
+```javascript
+$('.textarea-expand').textareaExpand();
+```
+
+Done. But you do have options, which you can add like this:
+```javascript
+$('.textarea-expand').textareaExpand({
+	'lineOffset' : 3
+});
+```
+
+### Options
+* lineOffset. Integer. How many lines before the text reaches the bottom of the textarea to trigger the expand. Default 1.
+* numberOfLinesToAdd. Integer. How many lines to add to the text area when expanding. Default 2.
+* cloneID. String. The ID to give the clone element. Default 'textarea-clone'.
+* addCSS. Boolean. Whether or not to add plugin-specific CSS or not. Default true.
+
+## How does it work?
+So you've got your containing element. Firstly, we create a clone of the textarea as a sibling. This means that it will have all the same classes and styles as the actual textarea. We add some additional styles to the clone to make it invisible have no height, so that the scrollHeight of the clone will be the height of the text.
+
+Whenever you interact with the textarea, it copies the text into the clone, works out the height of the text and whether it's time to expand, and expands accordingly by setting the height of the clone to the scrollHeight of the clone.
+
+Simple but effective.
---
+++ textareaExpand.plugin.js
@@ -0,0 +1,133 @@
+(function($){
+
+	var TextareaExpand = function(element, options) {
+		var self = $(element);
+		var _this = this;
+
+		var settings = $.extend({
+			'lineOffset' : 1,
+			'numberOfLinesToAdd' : 2,
+			'cloneID' : 'textarea-clone',
+			'addCSS' : true
+		}, options || {});
+
+		var textarea = self.find('textarea');
+		var textareaClone;
+
+		this.init = function() {
+			var _this = this;
+
+			// we set some CSS using JS because it's plugin-specific styles, but
+			// you can choose to not do this.
+			if (settings.addCSS) {
+				_this.setInitialCSS();
+			}
+
+			// create a hidden clone of the textarea.
+			_this.createClone();
+
+			// on every interacting with the input...
+			self.on('input', function() {
+
+				// make sure the clone matches the current text.
+				_this.copyTextIntoClone();
+
+				// and expand if we're running out of space.
+				if (_this.needToExpand()) {
+					_this.expand();
+				}
+			})
+
+			return _this;
+		}
+
+		this.setInitialCSS = function() {
+
+			var initialCSS = {
+				'position' : 'relative'
+			};
+
+			self.css(initialCSS);
+		}
+
+		this.createClone = function() {
+
+			var cloneCSS = {
+				'visibility' : 'hidden',
+				'position' : 'absolute',
+				'left' : 0,
+				'top' : 0,
+				'overflow' : 'hidden'
+			};
+
+			// create the clone textarea next to the original, inside self. This
+			// way it will have all the same styles.
+			textarea.after(textarea.clone().attr('id', settings.cloneID));
+
+			textareaClone = self.find('#' + settings.cloneID);
+
+			if (settings.addCSS) {
+				textareaClone.css(cloneCSS);
+			}
+		}
+
+		this.copyTextIntoClone = function() {
+
+			textareaClone.val( textarea.val() );
+
+		}
+
+		// returns true if the textarea is getting too full.
+		this.needToExpand = function() {
+			var _this = this;
+
+			var lineOffset = _this.getLineHeight(parseInt(settings.lineOffset));
+
+			var heightOfText = parseInt(textareaClone[0].scrollHeight);
+
+			var heightOfTextarea = parseInt(textarea.innerHeight());
+
+			if ( (heightOfText + lineOffset) > heightOfTextarea ) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+		// changes the height of the textarea to the height of the text, plus
+		// any extra height specified in numberOfLinesToAdd.
+		this.expand = function() {
+			var _this = this;
+
+			var heightToAdd = _this.getLineHeight(parseInt(settings.numberOfLinesToAdd));
+
+			textarea.height(textareaClone[0].scrollHeight + heightToAdd);
+
+			return _this;
+		}
+
+		this.getLineHeight = function(x) {
+			var lineHeight = parseInt(textarea.css('font-size'));
+
+			return x * lineHeight;
+		}
+
+	};
+
+	$.fn.textareaExpand = function(options) {
+		return this.each(function() {
+			var element = $(this);
+
+			// Return early if this element already has a plugin instance
+			if (element.data('textareaExpand')) return;
+
+			// pass options to plugin constructor
+			var textareaExpand = new TextareaExpand(this, options);
+
+			textareaExpand.init();
+
+			// Store plugin object in this element's data
+			element.data('textareaExpand', textareaExpand);
+		});
+	};
+})(jQuery);
